5b1f1673ec86677dbeb66613149dbb215014b2ee
innovateX <adivshared@gmail.com>
2026-05-26 12:08:04 +0530

    Delete src directory

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://fsoixtvttnzdoceaxqxg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzb2l4dHZ0dG56ZG9jZWF4cXhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5OTQ1MDAsImV4cCI6MjA5NDU3MDUwMH0.RbRzxW1g3yhaLIQJqToRQuKaqs3V-aqQbUqs78wVlJE'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
